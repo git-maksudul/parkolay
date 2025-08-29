4a6382299097d77f7848011020a5ef1871c1fb88
git-maksudul <maksudul.haque@devxhub.com>
2025-08-29 16:32:29 +0600

    Page52 completed

--- src/components/Page52Body/Page52Body.tsx
+++ src/components/Page52Body/Page52Body.tsx
@@ -1,5 +1,4 @@
-import img1 from '../../../public/images/page51/Rectangle1.png'
-import img2 from '../../../public/images/page51/Rectangle2.png'
+import img1 from '../../../public/images/page52/Rectangle.png'
 import car1 from '../../../public/images/car1.png'
 import car2 from '../../../public/images/car2.png'
 
@@ -8,59 +7,58 @@ const Page52Body = () => {
     return (
         <div className='lg:mx-96'>
             <div className="font-inter lg:flex lg:-ml-[200px] mt-8 lg:mt-0">
-                <div className='lg:w-7/8 w-5/6 mx-auto lg:mt-4 lg:mx-14'>
-                    <img className='lg:mx-6' src={img1} alt="" />
-                    <img className='lg:mx-4 lg:mt-6' src={img2} alt="" />
+                <div className=' w-full mx-auto lg:mt-4 lg:mx-14'>
+                    <img className='lg:-mx-16 lg:w-[102%] lg:h-[105%] lg:max-w-[1000px]' src={img1} alt="" />
                 </div>
 
-                <div className='flex gap-5 lg:gap-5 lg:grid lg:w-1/8 mt-9 lg:-mt-4 lg:mx-[25px]'>
+                <div className='flex gap-5 lg:gap-5 lg:grid lg:w-1/8 mt-9 lg:mt-0 lg:mx-[5px]'>
                     <div className="py-2 w-full max-w-md mx-auto space-y-1.5 lg:space-y-0">
                         <h2 className='lg:text-[16px] text-[10px] font-bold'>CONFIGURATION</h2>
                         <div className='flex text-[10px] lg:text-[16px] justify-between mt-1 lg:mt-2'>
                             <p>LIFT WITH TURNTABLE</p>
-                            <p>4</p>
+                            <p>10</p>
                         </div>
                         <div>
                             <hr className='border-[0.5px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>ENTRY/EXIT ROOM</p>
-                            <p>4</p>
+                            <p>10</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>SHELVES</p>
-                            <p>100</p>
+                            <p>177</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>4 ROWS</p>
-                            <p>84</p>
+                            <p>70</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>3 ROWS</p>
-                            <p>12</p>
+                            <p>5</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>1 ROWS</p>
-                            <p>4</p>
+                            <p>87</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>BUFFER CAPACITY</p>
-                            <p>36</p>
+                            <p>45</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
@@ -70,35 +68,35 @@ const Page52Body = () => {
                         <h2 className='lg:text-[16px] text-[10px] font-bold'>PERFORMANCE*</h2>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2'>
                             <p>MINS. EXIT TIME</p>
-                            <p>139 s.</p>
+                            <p>90 s.</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>AVERAGE EXIT TIME</p>
-                            <p>166 s.</p>
+                            <p>135 s.</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>SIMULTANEOUS OPERATION</p>
-                            <p>4 cars</p>
+                            <p>10 cars</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>HOURLY OPERATION</p>
-                            <p>87 cars</p>
+                            <p>240 cars</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full' />
                         </div>
                         <div className='flex text-[10px] mt-1 lg:text-[16px] justify-between lg:mt-2.5'>
                             <p>FULL EVACUATION TIME</p>
-                            <p>4h. 20mins.</p>
+                            <p>4h. 50mins.</p>
                         </div>
                         <div>
                             <hr className='lg:border-[0.5px] border-[0.24px] border-[#AFABAB] lg:mt-2.5 w-full lg:w-[405px]' />
@@ -107,21 +105,21 @@ const Page52Body = () => {
                     </div>
                 </div>
             </div>
-            <div className='mt-9 lg:mt-5 flex justify-center lg:justify-end mx-0 lg:-mx-[245px]'>
+            <div className='mt-9 lg:mt-5 flex justify-center lg:justify-end mx-0 lg:-mx-[260px]'>
                 <h2 className='lg:text-[16px] text-[10px] font-bold lg:-mx-34 mx-4'>CAR SIZES</h2>
                 <div className='lg:mt-10 mt-6 flex items-center lg:gap-[73px] gap-8 lg:mx-18 -ml-18'>
                     <div>
                         <img className='w-[68px] lg:w-[120px]' src={car1} alt="" />
                         <div className='flex items-baseline gap-1 lg:gap-2 mt-2 lg:mt-3'>
                             <p className='lg:text-[16px] text-[10px]'>H:200 cmx</p>
-                            <p className='lg:text-[25px] font-semibold'>0</p>
+                            <p className='lg:text-[25px] font-semibold'>90</p>
                         </div>
                     </div>
                     <div className='lg:mt-4'>
                         <img className='w-[68px] lg:w-[120px]' src={car2} alt="" />
                         <div className='flex items-baseline gap-1 lg:gap-2 mt-3 lg:mt-4'>
                             <p className='lg:text-[16px] text-[10px]'>H:160 cmx</p>
-                            <p className='lg:text-[25px] font-semibold'>375</p>
+                            <p className='lg:text-[25px] font-semibold'>348</p>
                         </div>
                     </div>
                 </div>
